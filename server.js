fc37e8d8239174a312b2977a96f0561ecb6f4f7e
Naimur Rahman <naimur978@gmail.com>
2020-08-07 11:56:48 +0600

    update port

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require("express");
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
