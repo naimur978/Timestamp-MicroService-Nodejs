052d3c4b9262284dae11562487b75cabe5a1e94f
Naimur Rahman <naimur978@gmail.com>
2020-08-10 11:36:34 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,68 +2,60 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use((req, res, next) => {
-  console.log(`${new Date()}:\n${req.originalUrl}, ${req.body}`);
-  next();
-})
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-//
-const jsonFromDate = date => {
-  return { unix: date.getTime(), utc: date.toUTCString() }
-}
-
-// get present time
-app.get('/api/timestamp', (req, res) => {
-  date = new Date();
-  res.json(jsonFromDate(date))
-})
-
-// get time from :date_string
-app.get('/api/timestamp/:date_string', (req, res) => {
+// your first API endpoint...
+app.get("/api/timestamp/:date_string", function(req, res) {
+  //parse date_string from api endpoint
   const dateString = req.params.date_string;
-  const time = Date.parse(dateString);
 
-  if (!isNaN(time)) {
-    // valid ISO string
-    res.json(jsonFromDate(new Date(time)));
-  } else {
-    // time is NaN: check if integer
-    const dateParsed = parseFloat(dateString);
-    if (Number.isInteger(dateParsed)) {
-      res.json(jsonFromDate(new Date(dateParsed)));
-    } else {
-      // not an integer
-      res.json({ error: "Invalid Date" });
-    }
+  // if no data resides in dateString
+  if (!dateString) {
+    return res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString()
+    });
   }
 
-})
 
+  // convert the date as number
+  // Number object : https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_number
+  const unix = Number(dateString);
+
+  if (unix) {
+    const date = new Date(unix).toUTCString();
+    if (!date) return res.json({ error: "Invalid Date" });
+
+    res.json({
+      unix: unix,
+      utc: new Date(unix).toUTCString()
+    });
+  } else {
+    const date = new Date(dateString).getTime();
+    if (!date) return res.json({ error: "Invalid Date" });
+
+    res.json({
+      unix: date,
+      utc: new Date(dateString).toUTCString()
+    });
+  }
+});
 
 // listen for requests :)
-const PORT = process.env.PORT || 3000;
-var listener = app.listen(PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
